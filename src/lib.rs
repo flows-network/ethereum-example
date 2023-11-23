be8e00ff18f8d008551b4856a0951aa73ddc1be1
grorge <g97116@yahoo.com>
2023-11-23 22:04:37 +0800

    feat: add pbm pay router

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use ethers_core::types::NameOrAddress;
 use webhook_flows::{create_endpoint, request_handler, send_response, route::{get, route, RouteError, Router}};
 use flowsnet_platform_sdk::logger;
 use serde_json::Value;
@@ -6,8 +5,8 @@ use serde_json::json;
 use std::collections::HashMap;
 use std::str::FromStr;
 use ethers_signers::{LocalWallet, Signer, MnemonicBuilder, coins_bip39::English};
-use ethers_core::types::{Bytes, U256, U64, H160};
-use ethers_core::{types::TransactionRequest, types::transaction::eip2718::TypedTransaction};
+use ethers_core::types::{NameOrAddress, Bytes, U256, U64, H160, TransactionRequest, transaction::eip2718::TypedTransaction};
+use ethers_core::abi::{Abi, Function, Token};
 use ethers_core::utils::hex;
 use ethers_core::rand;
 use hyper::{Client, Body};
@@ -35,6 +34,13 @@ async fn handler(_headers: Vec<(String, String)>, _subpath: String, _qry: HashMa
         .insert(
             "/gen-key",
             vec![get(gen_key)],
+        )
+        .unwrap();
+
+    router
+        .insert(
+            "/pbm-pay",
+            vec![get(pbm_pay)],
         )
         .unwrap();
 
@@ -93,20 +99,94 @@ async fn send_transaction(_headers: Vec<(String, String)>, _qry: HashMap<String,
     .with_chain_id(chain_id);
 
 
-    let address_from = wallet.address();
     let address_to = NameOrAddress::from(H160::from_str(_qry.get("address_to").expect("Require address_to").to_string().as_str().trim_matches('"')).expect("Failed to parse address_to"));
     let mut value = U256::from_dec_str("0").unwrap();
     if let Some(_value) = _qry.get("value") {
         value = U256::from_dec_str(_qry.get("value").unwrap_or(&Value::Number(0.into())).as_str().unwrap().trim_matches('"')).expect("Failed to parse value.");
     }
-    let nonce = get_nonce(&rpc_node_url, format!("{:?}", wallet.address()).as_str()).await.unwrap();
     let mut data = Bytes::from(vec![0u8; 32]);
     if let Some(qry_data) = _qry.get("data") {      
         data = Bytes::from(hex::decode(qry_data.to_string().trim_matches('"').trim_start_matches("0x")).expect("Failed to parse data."));
     }
 
-    log::info!("Parameter: {:#?} {:#?} {:#?}", data, nonce, address_to);
+    log::info!("Parameter: {:#?} {:#?}", data, address_to);
+
+    let params = json!([wrap_transaction(&rpc_node_url, chain_id, wallet, address_to, data, value).await.unwrap().as_str()]);
+    let resp =json_rpc(&rpc_node_url, "eth_sendRawTransaction", params).await.expect("Failed to send raw transaction.");
+
+    log::info!("resp: {:#?}", resp);
+
+    send_response(
+        200,
+        vec![(String::from("content-type"), String::from("text/html"))],
+        resp.into_bytes().to_vec(),
+    );
+}
+
+async fn pbm_pay(_headers: Vec<(String, String)>, _qry: HashMap<String, Value>, _body: Vec<u8>){
+    logger::init();
+    log::info!("PBM pay Query -- {:?}", _qry);
     
+    let rpc_node_url = std::env::var("RPC_NODE_URL").unwrap_or("https://sepolia-rollup.arbitrum.io/rpc".to_string());
+    let chain_id = std::env::var("CHAIN_ID").unwrap_or("421614".to_string()).parse::<u64>().unwrap_or(421614u64);
+    let private_key = std::env::var("PRIVATE_KEY").unwrap_or("".to_string());
+    let wallet: LocalWallet = private_key
+    .parse::<LocalWallet>()
+    .unwrap()
+    .with_chain_id(chain_id);
+
+
+    let reciver = NameOrAddress::from(H160::from_str(_qry.get("pay-to").expect("Require pay to address").to_string().as_str().trim_matches('"')).expect("Failed to parse address"));
+    let contract_addrss = NameOrAddress::from(H160::from_str(std::env::var("CONTRACT_ADDRESS").unwrap_or("0x2ba7EA93b29286CB1f65c151ea0ad97FcCD41C91".to_string()).as_str()).expect("Failed to parse contract address"));
+    let value = U256::from_dec_str("0").unwrap();
+    let wei_to_eth = U256::from_dec_str("1000000000000000000").unwrap();
+    let data = create_pbm_pay_data(reciver.as_address().unwrap().clone(), U256::from(10) * wei_to_eth).unwrap();
+
+    log::info!("Parameter: {:#?} {:#?}", data, reciver);
+
+    let params = json!([wrap_transaction(&rpc_node_url, chain_id, wallet, contract_addrss, data, value).await.unwrap().as_str()]);
+    let resp =json_rpc(&rpc_node_url, "eth_sendRawTransaction", params).await.expect("Failed to send raw transaction.");
+
+    log::info!("resp: {:#?}", resp);
+
+    send_response(
+        200,
+        vec![(String::from("content-type"), String::from("text/html"))],
+        resp.into_bytes().to_vec(),
+    );
+}
+
+pub fn create_pbm_pay_data(receiver_address: H160, amount: U256) -> Result<Bytes> {
+    
+    let contract_abi: &str = r#"[
+        {
+            "inputs": [
+                {"internalType": "address", "name": "receiver", "type": "address"},
+                {"internalType": "uint256", "name": "amount", "type": "uint256"}
+            ],
+            "name": "pay",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ]"#;
+    let abi: Abi = serde_json::from_str(contract_abi).unwrap();
+    let function: &Function = abi
+        .functions()
+        .find(|&f| f.name == "pay")
+        .ok_or("Function not found in ABI")?;
+
+    let receiver: H160 = receiver_address;
+    let tokens = vec![Token::Address(receiver), Token::Uint(amount.into())];
+    let data = function.encode_input(&tokens).unwrap();
+
+    Ok(Bytes::from(data))
+}
+
+
+pub async fn wrap_transaction(rpc_node_url: &str, chain_id: u64, wallet: LocalWallet, address_to: NameOrAddress, data: Bytes, value: U256) -> Result<String> {
+    let address_from = wallet.address();
+    let nonce = get_nonce(&rpc_node_url, format!("{:?}", wallet.address()).as_str()).await.unwrap();
     let estimate_gas = get_estimate_gas(&rpc_node_url, format!("{:?}", address_from).as_str(), 
                                         format!("{:?}", address_to.as_address().expect("Failed to transfer address")).as_str(), 
                                         format!("0x{:x}", value).as_str(), format!("{:}", data).as_str())
@@ -123,21 +203,12 @@ async fn send_transaction(_headers: Vec<(String, String)>, _qry: HashMap<String,
     .data::<Bytes>(data.into())
     .value(value).into();    
     
-    
-    log::info!("Tx: {:#?}", tx);
-    
+    log::info!("Tx: {:#?}", tx); 
     
     let signature = wallet.sign_transaction(&tx).await.expect("Failed to sign.");
-    let params = json!([format!("0x{}", hex::encode(tx.rlp_signed(&signature))).as_str()]);
-    let resp = json_rpc(&rpc_node_url, "eth_sendRawTransaction", params).await.expect("Failed to send raw transaction.");
-	
-    log::info!("resp: {:#?}", resp);
     
-    send_response(
-        200,
-        vec![(String::from("content-type"), String::from("text/html"))],
-        resp.into_bytes().to_vec(),
-    );
+
+    Ok(format!("0x{}", hex::encode(tx.rlp_signed(&signature))))
 }
 
 pub async fn get_gas_price(rpc_node_url: &str) -> Result<U256> {
@@ -181,7 +252,7 @@ pub async fn json_rpc(url: &str, method: &str, params: Value) -> Result<String> 
     let body = hyper::body::to_bytes(res.into_body()).await?;
     let map: HashMap<String, serde_json::Value> = serde_json::from_str(&String::from_utf8(body.into())?)?;
     
-    if map.contains_key("error") {
+    if !map.contains_key("result"){
         log::error!("{} request body: {:#?}", method, json!({
             "jsonrpc": "2.0",
             "method": method,
